31cd173330fd14078e0cd3ac03a4af775c42b48e
fbold <fbold.dev@gmail.com>
2025-08-15 22:17:20 +0200

    back to precmd

--- dev.sh
+++ dev.sh
@@ -1,2 +1,2 @@
-npx @tailwindcss/cli -i ./static/styles/globals.css -o ./build/output.css --watch &
+npx @tailwindcss/cli -i ./static/styles/globals.css -o ./static/styles/output.css --watch &
 air -c ./.air.toml &
