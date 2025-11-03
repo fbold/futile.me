3a98760c54a6e2e84ac321278ab7cd1d5e1d6b5b
fbold <fbold.dev@gmail.com>
2025-11-04 00:23:41 +0100

    migrate helper connects to db and can run up migs

--- utils/migrate.go
+++ utils/migrate.go
@@ -2,19 +2,29 @@ package main
 
 import (
 	// "context"
+	"bufio"
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"strings"
 	"time"
 
 	// "github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
 )
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func main() {
-	err := godotenv.Load()
+	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
@@ -28,7 +38,7 @@ func main() {
 		fmt.Println("new migration:", *newName)
 		os.MkdirAll("db/migrations", 0777)
 		if err != nil {
-			fmt.Println("new migration:", err)
+			log.Println("new migration:", err)
 		}
 		filename := time.Now().Format("03040502012006")
 		filename = strings.Join([]string{filename, *newName}, "_")
@@ -36,14 +46,94 @@ func main() {
 		filenameDown := strings.Join([]string{"db/migrations/", filename, ".down.sql"}, "")
 		os.WriteFile(filenameUp, []byte{}, 0666)
 		os.WriteFile(filenameDown, []byte{}, 0666)
+
+	case "run":
+		migrationsToRun := []string{}
+		direction := os.Args[2]
+		if direction == "up" {
+			migrationsToRun = findMigrationsToRun(direction)
+		} else if direction == "down" {
+			migrationsToRun = findMigrationsToRun(direction)
+		} else {
+			log.Fatal("Must specify migration direction (up / down)")
+		}
+
+		log.Println("MIGRATIONS TO RUN:")
+		for _, m := range migrationsToRun {
+			log.Println(m)
+		}
+
+		db := connectDB()
+		for _, migration := range migrationsToRun {
+			log.Printf("RUNNING MIGRATION: %s", migration)
+			dat, err := os.ReadFile(fmt.Sprintf("db/migrations/%s", migration))
+			check(err)
+			_, errdb := db.Exec(context.Background(), string(dat))
+			check(errdb)
+			migrationName := strings.TrimSuffix(migration, fmt.Sprintf(".%s.sql", direction))
+			os.WriteFile("db/migrations/tally", []byte(migrationName), 0666)
+			log.Println("DONE ✅️")
+		}
+	}
+}
+
+func findMigrationsToRun(direction string) []string {
+	ls, err := os.ReadDir("db/migrations")
+	check(err)
+
+	// holds all the migration filenames (up and down)
+	// look like this: 07575503112025_test.up.sql
+	migrations := []string{}
+	for _, line := range ls {
+		if strings.HasSuffix(line.Name(), fmt.Sprintf("%s.sql", direction)) {
+			migrations = append(migrations, line.Name())
+		}
 	}
 
-	// dbpool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
-	// if err != nil {
-	// 	fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
-	// 	os.Exit(1)
-	// }
-	//
-	// defer dbpool.Close()
+	// tally file keeps track of which migrations have been run
+	// when migrate run is run, any from above that aren't here
+	// are run. each line looks like: 07575503112025_test
+	f, err := os.OpenFile("db/migrations/tally", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Println(err)
+	}
+	defer f.Close()
 
+	scanner := bufio.NewScanner(f)
+	migrationsAlreadyRun := []string{}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		migrationsAlreadyRun = append(migrationsAlreadyRun, line)
+	}
+
+	migrationsToRun := []string{}
+	re := regexp.MustCompile(`\..*\.sql`)
+	for _, mig := range migrations {
+		if len(migrationsAlreadyRun) == 0 {
+			if direction == "up" {
+				migrationsToRun = migrations
+			}
+			break
+		}
+		for _, doneMig := range migrationsAlreadyRun {
+			migName := re.ReplaceAllLiteralString(mig, "")
+			log.Println("comparing", mig, migName, doneMig)
+			if (migName != doneMig && direction == "up") ||
+				(migName == doneMig && direction == "down") {
+				migrationsToRun = append(migrationsToRun, mig)
+			}
+		}
+	}
+
+	return migrationsToRun
+}
+
+func connectDB() *pgxpool.Pool {
+	dbpool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("Unable to create connection pool: %v\n", err)
+	}
+
+	return dbpool
 }
